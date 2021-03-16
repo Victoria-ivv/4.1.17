5b0e857b8a4884bb569ab5b1832f92403ffcd1f5
Victoria-ivv <ivv83@me.com>
2021-03-16 17:28:51 +0300

    добавлен цикл for

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,31 +6,40 @@ class MainClass
 	{
 		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-		var color = Console.ReadLine();
-
-		switch (color)
+		//var color = Console.ReadLine();
+		for (int i=1; i<5; i++)
 		{
-			case "red": //если выберем красный
-				Console.BackgroundColor = ConsoleColor.Red;
-				Console.ForegroundColor = ConsoleColor.Black;
+			Console.WriteLine("Iteration{0}", i);
+			switch (Console.ReadLine())
+			{
+				case "red": //если выберем красный
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is red!");
-				break;
+					Console.WriteLine("Your color is red!");
+					break;
 
-			case "green": //если выберем зеленый
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.ForegroundColor = ConsoleColor.Black;
+				case "green": //если выберем зеленый
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is green!");
-				break;
+					Console.WriteLine("Your color is green!");
+					break;
 
-			case "cyan": //если выберем голубой
-			case "cyan":
-				Console.BackgroundColor = ConsoleColor.Cyan;
-				Console.ForegroundColor = ConsoleColor.Black;
+				case "cyan": //если выберем голубой
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is cyan!");
-				break;
+					Console.WriteLine("Your color is cyan!");
+					break;
+
+				default://если другой цвет
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Your color is yellow!");
+					break;
+			}
 		}
 	}
 }
